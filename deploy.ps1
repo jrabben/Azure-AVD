e0082ba990590fa5a3fb6194ba7bab30f0dc36cd
jrabben <joakim.rabben@cipher.no>
2021-11-22 10:31:19 +0100

    modify deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,13 @@
+# Windows
+# $templateFile = "C:\Users\joaki\.vscode\git_clone\Azure\avd-image-snapshot.json"
+# $parameterFile="C:\Users\joaki\.vscode\git_clone\Azure\avd-image-snapshot-param.json"
 
-$templateFile = "C:\Users\joaki\.vscode\git_clone\Azure\avd-image-snapshot.json"
-$parameterFile="C:\Users\joaki\.vscode\git_clone\Azure\avd-image-snapshot-param.json"
+
+# MacOS
+$templateFile = "/Users/joakim/git-clone/Azure-AVD/avd-image-snapshot.json"
+$parameterFile="/Users/joakim/git-clone/Azure-AVD/avd-image-snapshot-param.json"
+
+
 New-AzResourceGroupDeployment `
   -Name AVD-Image `
   -ResourceGroupName GT-RG-AVD `
